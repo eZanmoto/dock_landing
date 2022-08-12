8a38de0ec3d41db0edf048e04205a0d3eaed1431
Sean Kelleher <ezanmoto@gmail.com>
2022-08-12 13:28:23 +0000

    Add "Binaries" section to "Installation" section

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
 function main() {
-  const items = document.querySelectorAll('.getting-started li');
+  enableHighlighting(document.querySelectorAll('.getting-started li'), 'mouseover');
+  enableHighlighting(document.querySelectorAll('.installation li'), 'click');
+}
+
+function enableHighlighting(items, trigger) {
   for (const item of items) {
-    item.addEventListener('mouseover', (e) => {
+    item.addEventListener(trigger, (e) => {
       for (const item of items) {
         item.classList.remove('highlighted');
       }
